41988feec116070bce14eb60bf0963cae3efd29c
Alejandro Klever <alejandroklever.workon@gmail.com>
2023-03-23 19:24:13 -0400

    Initial Commit

---
+++ NationalParks/NationalParksApp.swift
@@ -0,0 +1,17 @@
+//
+//  NationalParksApp.swift
+//  NationalParks
+//
+//  Created by Alejandro Klever on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct NationalParksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
